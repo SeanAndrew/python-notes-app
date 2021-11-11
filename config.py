f3e43d06e416de1593d25a3af18b3c5dd6b7e1ea
1s9chapman <sean@chapmancomputertech.com>
2021-11-11 13:47:58 -0500

    updated config file to use port 80

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ db_host = os.environ.get('DB_HOST', default='192.168.56.102')
 db_name = os.environ.get('DB_NAME', default='notes')
 db_user = os.environ.get('DB_USERNAME', default='sean')
 db_password = os.environ.get('DB_PASSWORD', default='')
-db_port = os.environ.get('DB_PORT', default='5432')
+db_port = os.environ.get('DB_PORT', default='80')
 
 SQLALCHEMY_TRACK_MODIFICATIONS= False
 SQLALCHEMY_DATABASE_URI = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
